c6d3057949066c2ac52602aa43742907ab5d0c44
nhatthang270404@gmail.com <nhatthang270404@gmail.com>
2024-06-07 01:34:46 +0700

    Add Size and ProductSizeTable, Change Constraint

--- database/MangoSneakerDB_Schema.sql
+++ database/MangoSneakerDB_Schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE Customer (
   LastName  varchar(50) NOT NULL, 
   Mail      varchar(50) NOT NULL UNIQUE, 
   Username  varchar(50) NOT NULL UNIQUE, 
-  Password  varchar(50) NOT NULL, 
+  [Password]  varchar(50) NOT NULL, 
   Phone     varchar(10) NOT NULL UNIQUE, 
   PRIMARY KEY (ID));
 CREATE TABLE [Order] (
@@ -36,22 +36,49 @@ CREATE TABLE Product (
   ID          int IDENTITY NOT NULL, 
   CategoryID  int NOT NULL, 
   ProductName varchar(50) NOT NULL UNIQUE, 
-  Quantity    int NOT NULL, 
   Price       float(10) NOT NULL, 
-  Image       varchar(50) NOT NULL, 
+  [Image]       varchar(50) NOT NULL, 
   Discount    float(5) NOT NULL, 
   PRIMARY KEY (ID));
+CREATE TABLE ProductSize (
+  ProductID  int NOT NULL, 
+  SizeNumber int NOT NULL, 
+  Quantity   int NOT NULL, 
+  PRIMARY KEY (ProductID, 
+  SizeNumber));
+CREATE TABLE [Size] (
+  SizeNumber int NOT NULL, 
+  PRIMARY KEY (SizeNumber));
+
 ALTER TABLE Product ADD CONSTRAINT FK_Category_Product_CategoryID FOREIGN KEY (CategoryID) REFERENCES Category (ID);
 ALTER TABLE CartDetail ADD CONSTRAINT FK_Customer_CartDetail_CustomerID FOREIGN KEY (CustomerID) REFERENCES Customer (ID);
 ALTER TABLE [Order] ADD CONSTRAINT FK_Customer_Order_CustomerID FOREIGN KEY (CustomerID) REFERENCES Customer (ID);
 ALTER TABLE OrderDetail ADD CONSTRAINT FK_Order_OrderDetail_OrderID FOREIGN KEY (OrderID) REFERENCES [Order] (ID);
 ALTER TABLE CartDetail ADD CONSTRAINT FK_Product_CartDetail_ProductID FOREIGN KEY (ProductID) REFERENCES Product (ID);
 ALTER TABLE OrderDetail ADD CONSTRAINT FK_Product_OrderDetail_ProductID FOREIGN KEY (ProductID) REFERENCES Product (ID);
+ALTER TABLE ProductSize ADD CONSTRAINT FK_Product_ProductSize_ProductID FOREIGN KEY (ProductID) REFERENCES Product (ID);
+ALTER TABLE ProductSize ADD CONSTRAINT FK_Size_ProductSize_SizeNumber FOREIGN KEY (SizeNumber) REFERENCES [Size] (SizeNumber);
+
+ALTER TABLE CartDetail
+ADD CONSTRAINT CK_CartDetail_Quantity_Positive CHECK (Quantity > 0)
+
+ALTER TABLE OrderDetail
+ADD CONSTRAINT CK_OrderDetail_Quantity_Positive CHECK (Quantity > 0)
+
+ALTER TABLE OrderDetail
+ADD CONSTRAINT CK_OrderDetail_UnitPrice_Positive CHECK (UnitPrice > 0)
+
+ALTER TABLE Product
+ADD CONSTRAINT CK_Product_Price_Positive CHECK (Price > 0)
+
+ALTER TABLE Product
+ADD CONSTRAINT CK_Product_Discount_NonNegative CHECK (Discount >= 0)
+
+ALTER TABLE ProductSize
+ADD CONSTRAINT CK_ProductSize_Quantity_Positive CHECK (Quantity > 0)
 
 
 
 
-
-
-DROP DATABASE MangoSneakerDB
+--DROP DATABASE MangoSneakerDB
 
--- database/insert_data.sql
+++ database/insert_data.sql
@@ -21,22 +21,22 @@ INSERT INTO Customer (FirstName, LastName, Mail, Username, Password, Phone) VALU
 ('Leo', 'White', 'leo@example.com', 'leowhite', 'password14', '0999857466'),
 ('Mona', 'Martin', 'mona@example.com', 'monamartin', 'password15', '0111118273');
 
-INSERT INTO Product (CategoryID, ProductName, Quantity, Price, Image, Discount) VALUES
-(1, 'Nike Air Max', 100, 150.00, 'nike_air_max.jpg', 10.0),
-(1, 'Nike Revolution', 200, 120.00, 'nike_revolution.jpg', 5.0),
-(2, 'Adidas Ultraboost', 150, 180.00, 'adidas_ultraboost.jpg', 15.0),
-(2, 'Adidas Superstar', 300, 110.00, 'adidas_superstar.jpg', 8.0),
-(3, 'Converse Chuck Taylor', 250, 70.00, 'converse_chuck_taylor.jpg', 12.0),
-(3, 'Converse All Star', 100, 75.00, 'converse_all_star.jpg', 10.0),
-(1, 'Nike Pegasus', 120, 130.00, 'nike_pegasus.jpg', 7.0),
-(1, 'Nike Cortez', 80, 90.00, 'nike_cortez.jpg', 10.0),
-(2, 'Adidas NMD', 90, 140.00, 'adidas_nmd.jpg', 5.0),
-(2, 'Adidas Stan Smith', 130, 95.00, 'adidas_stan_smith.jpg', 12.0),
-(3, 'Converse One Star', 70, 85.00, 'converse_one_star.jpg', 8.0),
-(3, 'Converse Pro Leather', 60, 100.00, 'converse_pro_leather.jpg', 10.0),
-(1, 'Nike Vapormax', 110, 200.00, 'nike_vapormax.jpg', 15.0),
-(2, 'Adidas Gazelle', 140, 85.00, 'adidas_gazelle.jpg', 10.0),
-(3, 'Converse Jack Purcell', 90, 95.00, 'converse_jack_purcell.jpg', 5.0);
+INSERT INTO Product (CategoryID, ProductName, Price, [Image], Discount) VALUES
+(1, 'Nike Air Max', 150.00, 'nike_air_max.jpg', 10.0),
+(1, 'Nike Revolution', 120.00, 'nike_revolution.jpg', 5.0),
+(2, 'Adidas Ultraboost', 180.00, 'adidas_ultraboost.jpg', 15.0),
+(2, 'Adidas Superstar', 110.00, 'adidas_superstar.jpg', 8.0),
+(3, 'Converse Chuck Taylor', 70.00, 'converse_chuck_taylor.jpg', 12.0),
+(3, 'Converse All Star', 75.00, 'converse_all_star.jpg', 10.0),
+(1, 'Nike Pegasus', 130.00, 'nike_pegasus.jpg', 7.0),
+(1, 'Nike Cortez', 90.00, 'nike_cortez.jpg', 10.0),
+(2, 'Adidas NMD', 140.00, 'adidas_nmd.jpg', 5.0),
+(2, 'Adidas Stan Smith', 95.00, 'adidas_stan_smith.jpg', 12.0),
+(3, 'Converse One Star', 85.00, 'converse_one_star.jpg', 8.0),
+(3, 'Converse Pro Leather', 100.00, 'converse_pro_leather.jpg', 10.0),
+(1, 'Nike Vapormax', 200.00, 'nike_vapormax.jpg', 15.0),
+(2, 'Adidas Gazelle', 85.00, 'adidas_gazelle.jpg', 10.0),
+(3, 'Converse Jack Purcell', 95.00, 'converse_jack_purcell.jpg', 5.0);
 
 INSERT INTO [Order] (CustomerID, [Date]) VALUES
 (1, '2024-05-01'),
@@ -88,6 +88,27 @@ INSERT INTO CartDetail (CustomerID, ProductID, Quantity) VALUES
 (13, 12, 1),
 (14, 14, 1),
 (15, 15, 2);
+
+INSERT INTO Size (SizeNumber) VALUES
+(36), (37), (38), (39), (40)
+
+INSERT INTO ProductSize (ProductID, SizeNumber, Quantity) VALUES
+(1, 36, 5), (1, 37, 10), (1, 38, 1), (1, 39, 1), (1, 40, 4),
+(2, 36, 7), (2, 37, 3), (2, 38, 6), (2, 39, 8), (2, 40, 2),
+(3, 36, 4), (3, 37, 9), (3, 38, 7), (3, 39, 2), (3, 40, 5),
+(4, 36, 1), (4, 37, 8), (4, 38, 4), (4, 39, 10), (4, 40, 3),
+(5, 36, 9), (5, 37, 6), (5, 38, 1), (5, 39, 3), (5, 40, 7),
+(6, 36, 2), (6, 37, 4), (6, 38, 8), (6, 39, 9), (6, 40, 6),
+(7, 36, 5), (7, 37, 1), (7, 38, 10), (7, 39, 7), (7, 40, 2),
+(8, 36, 3), (8, 37, 9), (8, 38, 2), (8, 39, 6), (8, 40, 4),
+(9, 36, 8), (9, 37, 7), (9, 38, 3), (9, 39, 1), (9, 40, 5),
+(10, 36, 6), (10, 37, 10), (10, 38, 5), (10, 39, 4), (10, 40, 9),
+(11, 36, 2), (11, 37, 7), (11, 38, 6), (11, 39, 8), (11, 40, 3),
+(12, 36, 5), (12, 37, 4), (12, 38, 1), (12, 39, 9), (12, 40, 7),
+(13, 36, 10), (13, 37, 3), (13, 38, 8), (13, 39, 2), (13, 40, 6),
+(14, 36, 7), (14, 37, 1), (14, 38, 9), (14, 39, 4), (14, 40, 5),
+(15, 36, 8), (15, 37, 10), (15, 38, 2), (15, 39, 5), (15, 40, 3);
+
 
 SELECT * FROM Category
 SELECT * FROM Customer
